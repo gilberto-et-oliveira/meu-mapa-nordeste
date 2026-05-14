dc61b5cdf9b124f2934a59f1da1812017ba2e889
gilberto-et-oliveira <80414407+gilberto-et-oliveira@users.noreply.github.com>
2026-05-14 10:07:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,69 @@
+// 1. Configuração inicial
 var map = L.map('map').setView([-9.66, -35.70], 11);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
 
-// Criamos o controle de rota de forma simples
+// 2. Configuração da Rota (Igual ao Google Maps)
 var controlRotas = L.Routing.control({
     waypoints: [],
     routeWhileDragging: true,
-    language: 'pt-BR'
+    reverseWaypoints: true,
+    showAlternatives: true,
+    language: 'pt-BR',
+    geocoder: L.Control.Geocoder.nominatim()
 }).addTo(map);
 
-function carregarPontos() {
-    fetch('pontos.geojson')
-        .then(response => response.json())
-        .then(data => {
-            L.geoJSON(data, {
-                onEachFeature: function (feature, layer) {
-                    // Pegamos as coordenadas do GeoJSON [Long, Lat]
+// 3. FUNÇÃO MÁGICA: Clique com o botão direito para definir pontos
+map.on('contextmenu', function(e) {
+    var container = L.DomUtil.create('div'),
+        startBtn = L.DomUtil.create('button', '', container),
+        destBtn = L.DomUtil.create('button', '', container);
+
+    startBtn.innerHTML = 'Partir daqui';
+    destBtn.innerHTML = 'Ir para cá';
+
+    L.DomEvent.on(startBtn, 'click', function() {
+        controlRotas.spliceWaypoints(0, 1, e.latlng);
+        map.closePopup();
+    });
+
+    L.DomEvent.on(destBtn, 'click', function() {
+        var wps = controlRotas.getWaypoints();
+        controlRotas.spliceWaypoints(wps.length - 1, 1, e.latlng);
+        map.closePopup();
+    });
+
+    L.popup().setContent(container).setLatLng(e.latlng).openOn(map);
+});
+
+// 4. Carregar seus pontos do GeoJSON
+fetch('pontos.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            onEachFeature: function (feature, layer) {
+                layer.on('click', function(e) {
                     var coords = feature.geometry.coordinates;
-                    var nome = feature.properties.nome || "Estabelecimento";
+                    var nome = feature.properties.nome || "Ponto";
                     
-                    // Criamos o botão passando Lat e Long corretamente
-                    var btn = "<button onclick='tracarRota(" + coords[1] + "," + coords[0] + ")'>Ir para cá</button>";
-                    layer.bindPopup("<b>" + nome + "</b><br>" + btn);
-                }
-            }).addTo(map);
-        });
-}
+                    // Ao clicar no ponto, abre um popup com opções de rota
+                    var container = L.DomUtil.create('div');
+                    container.innerHTML = "<b>" + nome + "</b><br>";
+                    
+                    var btnDestino = L.DomUtil.create('button', '', container);
+                    btnDestino.innerHTML = "Traçar rota para este ponto";
+                    
+                    L.DomEvent.on(btnDestino, 'click', function() {
+                        var destino = L.latLng(coords[1], coords[0]);
+                        var wps = controlRotas.getWaypoints();
+                        controlRotas.spliceWaypoints(wps.length - 1, 1, destino);
+                        map.closePopup();
+                    });
 
-function tracarRota(lat, lng) {
-    var waypointDestino = L.latLng(lat, lng);
-    controlRotas.spliceWaypoints(controlRotas.getWaypoints().length - 1, 1, waypointDestino);
-}
-
-carregarPontos();
+                    layer.bindPopup(container).openPopup();
+                });
+            }
+        }).addTo(map);
+    });
