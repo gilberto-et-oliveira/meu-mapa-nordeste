c8089a79341ad7261894c5c28bdb037df07fb5cf
gilberto-et-oliveira <80414407+gilberto-et-oliveira@users.noreply.github.com>
2026-05-14 09:52:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,41 +7,44 @@ L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 }).addTo(map);
 
-// 3. Controle de Rotas
+// 3. Controle de Rotas (Versão simplificada e estável)
 var controlRotas = L.Routing.control({
     waypoints: [],
     routeWhileDragging: true,
     language: 'pt-BR',
-    show: true, // Isso garante que o painel apareça
-    collapsible: true, // Permite minimizar o painel
-    geocoder: L.Control.Geocoder ? L.Control.Geocoder.nominatim() : null 
+    show: true
 }).addTo(map);
 
 // 4. Função para carregar as camadas
 function carregarCamada(arquivo, estilo, popupColuna) {
     fetch(arquivo)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) throw new Error("Erro ao carregar " + arquivo);
+            return response.json();
+        })
         .then(data => {
             L.geoJSON(data, {
                 style: estilo,
                 onEachFeature: function (feature, layer) {
-                    // Texto do Popup baseado nas colunas que você passou
-                    var conteudo = "<b>" + (feature.properties[popupColuna] || "Informação") + "</b>";
+                    var nome = feature.properties[popupColuna] || "Informação";
+                    var conteudo = "<b>" + nome + "</b>";
                     
-                    // Se for a camada de pontos, adiciona o botão de rota
                     if (arquivo === 'pontos.geojson') {
-                        conteudo += "<br>" + (feature.properties.endereço || "");
+                        var endereco = feature.properties.endereço || "";
+                        conteudo += "<br>" + endereco;
+                        // O botão chama a função de rota
                         conteudo += "<br><br><button onclick='adicionarNaRota(" + feature.geometry.coordinates[1] + "," + feature.geometry.coordinates[0] + ")'>Adicionar à Rota</button>";
                     }
                     
                     layer.bindPopup(conteudo);
                 }
             }).addTo(map);
-        });
+        })
+        .catch(err => console.error(err));
 }
 
-// 5. Carrega os arquivos (Certifique-se de que os nomes no GitHub serão estes)
-//carregarCamada('estados.geojson', { color: "#333", weight: 2, fillOpacity: 0.1 }, 'NM_UF');
+// 5. Carrega os arquivos
+// carregarCamada('estados.geojson', { color: "#333", weight: 2, fillOpacity: 0.1 }, 'NM_UF');
 carregarCamada('municipios.geojson', { color: "#999", weight: 0.5, fillOpacity: 0 }, 'NM_MUN');
 carregarCamada('pontos.geojson', {}, 'nome');
 
@@ -50,14 +53,10 @@ function adicionarNaRota(lat, lng) {
     var atuais = controlRotas.getWaypoints();
     var novoPonto = L.latLng(lat, lng);
 
-    // Se o primeiro ponto estiver vazio, define como partida
     if (!atuais[0] || atuais[0].latLng == null) {
         controlRotas.spliceWaypoints(0, 1, novoPonto);
     } else {
-        // Adiciona como próximo destino
         controlRotas.spliceWaypoints(atuais.length, 0, novoPonto);
     }
-    
-    // Abre o painel de rotas automaticamente
     controlRotas.show();
 }
